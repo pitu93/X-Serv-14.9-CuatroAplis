1c1b4c59bae5c33fd7fd9690cc2c5467f0045a0d
alorenzo <pitu_fuenla93@hotmail.es>
2015-03-04 17:45:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import webapp
-import sumador
+import suma
 import hola
 import aleat
 
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     anApp = webapp.app()
     otherApp = webapp.app()
     hola = hola.hola()
-    suma = sumador.suma()
+    suma = suma.suma()
     aleatorio = aleat.aleatorio()
     testWebApp = webapp.webApp("localhost", 1234, {'/app': anApp,
                                             '/other': otherApp,
